324a2527c64bec318ad937422a0a8b9817f0a35e
Vedran Lasic <vedro.lasic@gmail.com>
2018-12-28 20:09:16 +0100

    Created Employee class and Jobs enum

---
+++ Employees.Infrastructure/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Infrastructure
+{
+    public class EmployeeRepository
+    {
+        private List<Employee> _employeeRepository;
+
+        public EmployeeRepository()
+        {
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            _employeeRepository = new List<Employee>()
+            {
+                new Employee()
+
+            };
+        }
+    }
+}
