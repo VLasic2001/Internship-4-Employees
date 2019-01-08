1e6007cc92a11e72ffeb6cb0b71129fa6e082910
Vedran Lasic <vedro.lasic@gmail.com>
2019-01-08 21:14:41 +0100

    Created ProjectRepository

--- Employees.Data/Project.cs
+++ Employees.Data/Project.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Employees.Data
 {
-    class Project
+    public class Project
     {
+        public string ProjectName { get; set; }
+        public Dictionary<Employee, int> EmployeesOnProjectAndWorkHours { get; set; }
+        public DateTime ProjectStart { get; set; }
+        public DateTime ProjectFinish { get; set; }
+
+        public Project(string projectName, Dictionary<Employee, int> employeesOnProjectAndWorkHours, DateTime projectStart, DateTime projectFinish)
+        {
+            ProjectName = projectName;
+            EmployeesOnProjectAndWorkHours = employeesOnProjectAndWorkHours;
+            ProjectStart = projectStart;
+            ProjectFinish = projectFinish;
+        }
+
+        public override string ToString()
+        {
+            return $"{ProjectName} - {EmployeesOnProjectAndWorkHours.Count} employees working on this project.";
+        }
     }
 }
